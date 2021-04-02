84992528576da218db0e5bb85f6bdec2ee19c33d
André Treib <andre.treib@eldorado.org.br>
2021-04-02 20:16:08 -0300

    refactor: create file for exporting styled components

---
+++ src/pages/dashboard/DashboardPage.styles.tsx
@@ -0,0 +1,104 @@
+import styled from 'styled-components';
+import { colors } from '../../utils/theme/colors';
+
+export const Page = styled.div`
+  background-color: ${colors.background};
+  height: 100%;
+`;
+
+export const Header = styled.header`
+  padding: 1rem;
+  background-color: ${colors.primary};
+  color: ${colors.primaryContrast};
+
+  border-bottom-left-radius: 70% 2%;
+  border-bottom-right-radius: 100% 30%;
+`;
+
+export const ToolbarButton = styled.div``;
+export const Toolbar = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  ${ToolbarButton} {
+    border: 1px solid #ccc;
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+  }
+`;
+
+export const UserInfo = styled.div``;
+export const WeatherInfo = styled.div``;
+export const Summary = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 0;
+
+  ${UserInfo} {
+    h1 {
+      margin: 0.5rem;
+      margin-left: 0;
+      font-size: 1.5rem;
+    }
+
+    h2 {
+      margin: 0.5rem;
+      margin-left: 0;
+      font-size: 1.5rem;
+      line-height: 1.2;
+    }
+
+    p {
+      margin: 0.5rem;
+      margin-left: 0;
+      font-size: 1rem;
+    }
+  }
+
+  ${WeatherInfo} {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+  }
+`;
+
+export const SliderHeader = styled.h2``;
+export const SliderList = styled.div``;
+export const SliderItem = styled.div``;
+export const Slider = styled.section`
+  margin: 2rem 0;
+
+  ${SliderHeader} {
+    margin: 1rem;
+    color: ${colors.primary};
+  }
+
+  ${SliderList} {
+    width: 100%;
+    overflow: auto;
+    padding-bottom: 1rem;
+    margin: 0 1rem;
+    display: flex;
+    flex-wrap: no-wrap;
+    justify-content: flex-start;
+
+    ${SliderItem} {
+      min-width: 45%;
+      min-height: 100px;
+      padding: 1rem;
+      margin-right: 1rem;
+
+      border-radius: 4px;
+      background-color: ${colors.white};
+      box-shadow: 0px 2px 2px -2px ${colors.black};
+    }
+  }
+`;
--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,109 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
-import { colors } from '../../utils/theme/colors';
 import { UnstyledLink } from '../../utils/components/UnstyledLink';
-
-const Page = styled.div`
-  background-color: ${colors.background};
-  height: 100%;
-`;
-
-const Header = styled.header`
-  padding: 1rem;
-  background-color: ${colors.primary};
-  color: ${colors.primaryContrast};
-
-  border-bottom-left-radius: 70% 2%;
-  border-bottom-right-radius: 100% 30%;
-`;
-
-const ToolbarButton = styled.div``;
-const Toolbar = styled.div`
-  display: flex;
-  justify-content: space-between;
-
-  ${ToolbarButton} {
-    border: 1px solid #ccc;
-    border-radius: 50%;
-    width: 3rem;
-    height: 3rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden;
-  }
-`;
-
-const UserInfo = styled.div``;
-const WeatherInfo = styled.div``;
-const Summary = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem 0;
-
-  ${UserInfo} {
-    h1 {
-      margin: 0.5rem;
-      margin-left: 0;
-      font-size: 1.5rem;
-    }
-
-    h2 {
-      margin: 0.5rem;
-      margin-left: 0;
-      font-size: 1.5rem;
-      line-height: 1.2;
-    }
-
-    p {
-      margin: 0.5rem;
-      margin-left: 0;
-      font-size: 1rem;
-    }
-  }
-
-  ${WeatherInfo} {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-  }
-`;
-
-const SliderHeader = styled.h2``;
-const SliderList = styled.div``;
-const SliderItem = styled.div``;
-const Slider = styled.section`
-  margin: 2rem 0;
-
-  ${SliderHeader} {
-    margin: 1rem;
-    color: ${colors.primary};
-  }
-
-  ${SliderList} {
-    width: 100%;
-    overflow: auto;
-    padding-bottom: 1rem;
-    margin: 0 1rem;
-    display: flex;
-    flex-wrap: no-wrap;
-    justify-content: flex-start;
-
-    ${SliderItem} {
-      min-width: 45%;
-      min-height: 100px;
-      padding: 1rem;
-      margin-right: 1rem;
-
-      border-radius: 4px;
-      background-color: ${colors.white};
-      box-shadow: 0px 2px 2px -2px ${colors.black};
-    }
-  }
-`;
+import {
+  Page,
+  Header,
+  Toolbar,
+  ToolbarButton,
+  Summary,
+  UserInfo,
+  WeatherInfo,
+  Slider,
+  SliderHeader,
+  SliderItem,
+  SliderList,
+} from './DashboardPage.styles';
 
 export const DashboardPage = () => {
   return (
