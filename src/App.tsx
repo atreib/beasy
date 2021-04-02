92f3bef1938c756e5f793fab66dcc6a5fc6f1b84
André Treib <andre.treib@eldorado.org.br>
2021-04-01 22:02:36 -0300

    config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { AppRoutes, RoutesModel } from './routes';
+
+const NotFoundPage = () => {
+  return <h1>Página não encontrada</h1>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Suspense fallback={<h1>Carregando...</h1>}>
+          <Switch>
+            {AppRoutes &&
+              AppRoutes.map((route: RoutesModel) => (
+                <Route key={route.path} path={route.path} exact component={route.component} />
+              ))}
+            <Route component={NotFoundPage} />
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface RoutesModel {
+  path: string;
+  component: React.LazyExoticComponent<React.ComponentType<unknown>>;
+  title: string;
+}
+
+export const AppRoutes: Array<RoutesModel> = [
+  {
+    path: '/',
+    component: React.lazy(() => import('./../pages/home/HomePage')),
+    title: 'Início',
+  },
+];
